2a628251471569ac21f1a389ddc281a007f06f18
manaken0522 <manaken0522@gmail.com>
2024-06-11 00:13:01 +0900

    no message

---
+++ index.js
@@ -0,0 +1,38 @@
+const config = require('./config.json');
+const discord = require('discord.js');
+
+const client = new discord.Client({intents:[
+    discord.GatewayIntentBits.Guilds,
+    discord.GatewayIntentBits.GuildPresences
+]})
+
+client.once(discord.Events.ClientReady, (readyClient) => {
+    console.log('ready');
+});
+
+let monitoring_members = []
+
+client.on(discord.Events.PresenceUpdate, (oldPresence, newPresence) => {
+    if(config.targets.includes(String(newPresence.user.id))) {
+        if(newPresence.status === 'offline') {
+            if(!monitoring_members.includes(String(newPresence.user.id))) {
+                monitoring_members.push(String(newPresence.user.id));
+                setInterval(() => {
+                    if(monitoring_members.includes(String(newPresence.user.id))) {
+                        const alert_channel = client.channels.cache.find(channel => channel.id == config.alert_channel);
+                        alert_channel.send(`${newPresence.user.username}がダウンしています`);
+                    }
+                }, 10*1000);
+            }
+        }
+        else {
+            if(monitoring_members.includes(String(newPresence.user.id))) {
+                const alert_channel = client.channels.cache.find(channel => channel.id == config.alert_channel);
+                alert_channel.send(`${newPresence.user.username}が復旧しました`);
+                monitoring_members = monitoring_members.filter(target => target != newPresence.user.id);
+            }
+        }
+    }
+})
+
+client.login(config.token);
